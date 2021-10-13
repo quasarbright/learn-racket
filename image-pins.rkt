d26d4d16582ca19c83c11c1781453cffdc425007
Mike Delmonaco <mthundershock@gmail.com>
2021-10-13 14:02:42 -0400

    finish advanced image-pins macro

--- image-pins.rkt
+++ image-pins.rkt
@@ -1,47 +1,33 @@
 #lang racket
 (require pict)
 
+;; macro for pinning a sequence of images on top of each other.
+;; example usage:
 #;
-(pins bg
-      [pin x1 y1 img1]
-      [pin x2 y2 img2]
-      [pin x3 y3 img3])
-#;
-(pin-over (pin-over (pin-over bg x1 y1 img1) x2 y2 img2) x3 y3 img3)
-
-#;
-(pins bg
-      [pin x1 y1 img1]
-      [define k 2]
-      [pin k k img2])
-
-;; macro for pinning a sequence of images on top of each other
+(pins (colorize (filled-rectangle 70 40) "chocolate")
+      (define x 10)
+      (define y 10)
+      (pin x y (colorize (filled-rectangle 30 30) "orange"))
+      (pin 20 20 (colorize (disk 10) "white")))
 (define-syntax (pins stx)
   (syntax-case stx (pin)
-    [(_ bg (pin x y img) ...)
+    [(_ bg stmt ...)
      #'(begin
          (define acc bg)
-         (set! acc (pin-over acc x y img))
-         ...
-         acc)
-     ]))
-
-(define-syntax (pins- stx)
-  (syntax-case stx (pin)
-    [(_ bg stmt ...)
-     #`(begin
-         (define acc bg)
-         (pins-help acc stmt)
+         (pins-stmt acc stmt)
          ...
          acc)]))
 
-(define-syntax (pins-help stx)
+;; helper macro which runs a pins statement, which is a
+;; (pin x y img) or
+;; <expr>
+(define-syntax (pins-stmt stx)
   (syntax-case stx (pin)
-    [(_ acc (pin x y img)) (set! acc (pin-over acc x y img))]
-    [(_ acc stmt) stmt]))
-#;
-(pin-over (colorize (filled-rectangle 70 40) "chocolate")
-            10 10
-            (colorize (filled-rectangle 30 30) "orange"))
+    [(_ acc (pin x y img)) #'(set! acc (pin-over acc x y img))]
+    [(_ acc stmt) #'stmt]))
+
 (pins (colorize (filled-rectangle 70 40) "chocolate")
-      [pin 10 10 (colorize (filled-rectangle 30 30) "orange")])
+      (define x 10)
+      (define y 10)
+      (pin x y (colorize (filled-rectangle 30 30) "orange"))
+      (pin 20 20 (colorize (disk 10) "white")))
