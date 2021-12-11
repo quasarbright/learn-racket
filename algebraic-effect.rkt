29304d889a09f35be278b5fd92acbd91bd82fbe4
Mike Delmonaco <mthundershock@gmail.com>
2021-12-11 15:22:42 -0500

    labeled tests, test async, more thoughts

--- algebraic-effect.rkt
+++ algebraic-effect.rkt
@@ -1,13 +1,65 @@
 #lang racket
 
-;; An attempt at implementing algebraic effects using exceptions and continuations
+;; An implementation of algebraic effects using exceptions and continuations
+
+(require racket/control)
 
 (provide perform with-effect-handlers)
 
 (module+ test (require rackunit))
 
-;; An effect, which contains the performed effect eff and the continuation to resume to
-(struct effect (eff cont))
+#|
+
+The two essential forms are `perform` and `with-effect-handlers`.
+Example:
+> (with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
+    (* 3 (perform 4)))
+15
+
+It's like an exception, but you can resume from it with a value.
+However, it's more powerful than that.
+
+You can also get non-determinism:
+> (with-effect-handlers ([number? (λ (n resume-with) (list (resume-with (add1 n)) (resume-with (+ 2 n))))])
+    (perform 1))
+'(2 3)
+
+Async:
+> (touch (with-effect-handlers ([number? (λ (n resume-with) (future (thunk (resume-with (add1 n)))))])
+           (perform 1)))
+2
+
+I don't think multiple performs and multiple resumes makes sense. If you have multiple performs,
+thinking about the evaluation in terms of (resume-with v) returning the result of the resumed body
+might not even make sense, since that will involve another handling. Which one should be the result
+of the whole thing? It doesn't make sense.
+
+Maybe it does make sense. calling resume-with in the handler runs the rest of the body with that value
+filled in for the hole. This may include other performs. In that case, you'd end up coming back to the
+handler, and resuming. So you have to consider how the code will come back to the handler. This control
+flow is pretty confusing. It's kind of like foldr since the whole thing evaluates to the handling of the
+first perform, but 
+
+|#
+
+;; Perform an algebraic effect
+(define-syntax-rule (perform eff)
+  (shift k (raise (effect eff k))))
+
+;; Handle algebraic effects
+#;(with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
+    (* 3 (perform 4))) ; results in 15
+(define-syntax-rule (with-effect-handlers ([pred handler] ...) body ...)
+  (with-handlers ([(effect-predicate pred) (λ (e) (reset (handler (effect-eff e) (effect-cont e))))]
+                  ...)
+    (reset body ...)))
+
+(struct effect (eff cont) #:transparent)
+;; An [Effect A] is an (effect A Continuation)
+;; Interpretation: an (effect eff cont) represents a performed algebraic effect.
+;; eff is the value passed to `perform`
+;; cont is the continuation which, when applied to an argument `v`, resumes the computation at the
+;; performance site with the `perform` form replaced by `v`
 
 #; { (Any -> Boolean) -> (Any -> Boolean) }
 ;; Lifts a predicate on values to a predicate on effects containing those values
@@ -18,27 +70,45 @@
   (check-false ((effect-predicate number?) (effect 'foo #f)))
   (check-false ((effect-predicate number?) 'foo)))
 
-;; Perform an algebraic effect
-(define-syntax-rule (perform eff)
-  (let/cc k (raise (effect eff k))))
 
-;; Handle algebraic effects
-#;(with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
-    (* 3 (perform 4))) ; results in 5
-(define-syntax-rule (with-effect-handlers ([pred handler] ...) body ...)
-  (with-handlers ([(effect-predicate pred) (λ (e) (handler (effect-eff e) (effect-cont e)))]
-                  ...)
-    body ...))
-
+;; Test programs
 (module+ test
-  (check-equal? (with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
-                  (* 3 (perform 4)))
-                15))
-
-;; Problem: you can't do non-determinism
-;; Ex:
-#;(with-effect-handlers ([number? (λ (n resume-with) (list (resume-with (add1 n)) (resume-with (+ 2 n))))])
-    1)
-;; Want that to be (list 2 3)
-;; You might want some other control operators, maybe reset shift???
-;; With shift, you can call k multiple times
+  (test-equal? "simple perform"
+               (with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
+                 (* 3 (perform 4)))
+               15)
+  (test-equal? "2 performs"
+               (with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
+                 ; I think it loses the handler the second time around
+                 (list (perform 1) (perform 4)))
+               '(2 5))
+  (test-exn "unhandled effect"
+            effect?
+            (thunk (with-effect-handlers ([number? (λ (n resume-with) (resume-with n))])
+                     (perform 'foo))))
+  (test-equal? "ignored effect"
+               (with-effect-handlers ([number? (λ (n resume-with) 3)])
+                 (+ 2 (perform 500)))
+               3)
+  (test-equal? "ignored, no perform"
+               (with-effect-handlers ([number? (λ (n resume-with) 3)])
+                 4)
+               4)
+  (test-equal? "call a function which performs"
+               (let ([perform-five (thunk (perform 5))])
+                 (with-effect-handlers ([number? (λ (n resume-with) (resume-with (add1 n)))])
+                   (perform-five)))
+               6)
+  (test-equal? "non-determinism"
+               (with-effect-handlers ([number? (λ (n resume-with) (list (resume-with (add1 n)) (resume-with (+ 2 n))))])
+                 (perform 1))
+               '(2 3))
+  (test-equal? "non-determinism with two performs"
+               (with-effect-handlers ([number? (λ (n resume-with) (list (resume-with (add1 n)) (resume-with (+ 2 n))))])
+                 (list (perform 5) (perform 1)))
+               '(((5 2) (5 3))
+                 ((6 2) (6 3))))
+  (test-equal? "async"
+               (touch (with-effect-handlers ([number? (λ (n resume-with) (future (thunk (resume-with (add1 n)))))])
+                        (perform 1)))
+               2))
