d6f76174140705f195313fd42cc764ee071e9c84
Mike Delmonaco <mdelmona@amazon.com>
2023-06-01 17:32:50 -0400

    quotation, lambdas evaluate to procedures

--- dependent-types.rkt
+++ dependent-types.rkt
@@ -12,17 +12,20 @@
 ; An Expr is one of
 ; symbol (variable)
 ; (: Expr Type)
-; (lambda symbol Expr)
+; (lambda (symbol) Expr)
 ; (Expr Expr)
 
 
 ; A Value is one of
-; (lambda symbol Value)
+; (symbol -> Value)
 ; NeutralExpr
 
 ; A NeutralExpr is one of
 ; symbol
 ; (NeutralExpr Value)
+
+; An Env is a (hash symbol Value)
+(define empty-env (hasheq))
 
 ; A Context is a (hash symbol (or Type Kind))
 ; mapping to type is for term variables, mapping to kind is for type variables. don't worry about collision for now
@@ -30,28 +33,45 @@
 
 (module+ test (require (except-in rackunit check)))
 
-; Expr -> Value
-(define (eval expr)
-  (match expr
-    [(? symbol? expr) expr]
-    [`(: ,expr _) (eval expr)]
-    [`(lambda ,x ,body)
-     `(lambda ,x ,(eval body))]
-    [`(,rator ,rand)
-     (match (eval rator)
-       ; note body is a value, not an expr
-       [`(lambda ,x ,body) (eval (subst body x (eval rand)))]
-       ; TODO check if it's a neutral once there are numbers and stuff
-       [n `(,n ,(eval rand))])]))
+; Expr -> Expr
+(define (normalize expr) (quote-value (eval expr empty-env)))
 
-; Value symbol Value
-(define (subst v x replacement)
+; Expr -> Value
+(define (eval expr env)
+  (match expr
+    [(? symbol? expr) (hash-ref env expr expr)]
+    [`(: ,expr _) (eval expr env)]
+    [`(lambda (,x) ,body)
+     (lambda (v) (eval body (hash-set env x v)))]
+    [`(,rator ,rand)
+     (match (eval rator env)
+       [(and f (? procedure?)) (f (eval rand env))]
+       ; TODO check if it's a neutral once there are numbers and stuff
+       [n `(,n ,(eval rand env))])]))
+
+; Value -> Expr
+(define (quote-value v [count 0])
   (match v
-    [(? symbol? v) (if (eq? v x) replacement v)]
-    [`(lambda ,arg ,body)
-     `(lambda ,arg ,(if (eq? arg x)
-                        body
-                        (subst body x replacement)))]))
+    [(? symbol?) v]
+    [`(,n-rator ,v-rand)
+     `(,n-rator ,(quote-value v-rand count))]
+    [(? procedure?)
+     (define x (number->var count))
+     `(lambda (,x) ,(quote-value (v x) (add1 count)))]))
+
+(module+ test
+  (check-equal? (quote-value 'x) 'x)
+  (check-equal? (quote-value '(x y)) '(x y))
+  (check-equal? (quote-value (lambda (x) x)) '(lambda (_.0) _.0))
+  (check-equal? (quote-value (lambda (x) (lambda (y) x)))
+                '(lambda (_.0) (lambda (_.1) _.0))))
+
+; Natural -> symbol
+(define (number->var n)
+  (string->symbol (format "_.~a" n)))
+
+(module+ test
+  (check-equal? (number->var 0) '_.0))
 
 ; Expr Context -> Type
 (define (infer expr ctx)
@@ -62,7 +82,7 @@
      (check-type type ctx)
      (check expr type ctx)
      type]
-    [`(lambda ,x ,body) (error 'infer "cannot infer type of lambda")]
+    [`(lambda (,x) ,body) (error 'infer "cannot infer type of lambda")]
     [`(,rator ,rand)
      (define t-rator (infer rator ctx))
      (match t-rator
@@ -74,7 +94,7 @@
 ; Expr Type Context -> Void
 (define (check expr type ctx)
   (match expr
-    [`(lambda ,x ,body)
+    [`(lambda (,x) ,body)
      (match type
        [`(-> ,t-x ,t-body)
         (check body t-body (hash-set ctx x t-x))]
@@ -95,15 +115,16 @@
      (check-type t-ret ctx)]))
 
 (module+ test
-  (check-equal? (eval 'x) 'x)
-  (check-equal? (eval '(lambda x x)) '(lambda x x))
-  (check-equal? (eval '((lambda x x) y)) 'y)
+  (check-equal? (eval 'x empty-env) 'x)
+  (check-equal? (eval 'x (hasheq 'x 'y)) 'y)
+  (check-equal? (normalize '(lambda (x) x)) '(lambda (_.0) _.0))
+  (check-equal? (eval '((lambda (x) x) y) empty-env) 'y)
   (check-equal? (infer 'x (hasheq 'a '* 'x 'a)) 'a)
   (check-equal? (check 'x 'a (hasheq 'a '* 'x 'a)) (void))
-  (check-equal? (check '(lambda x x) '(-> a a) empty-ctx) (void))
-  (check-equal? (infer '(: (lambda x x) (-> a a)) (hasheq 'a '*)) '(-> a a))
-  (check-equal? (infer '((: (lambda x x) (-> a a)) y) (hasheq 'a '* 'y 'a)) 'a)
-  (check-equal? (infer '(((: (lambda x (lambda y x))
+  (check-equal? (check '(lambda (x) x) '(-> a a) empty-ctx) (void))
+  (check-equal? (infer '(: (lambda (x) x) (-> a a)) (hasheq 'a '*)) '(-> a a))
+  (check-equal? (infer '((: (lambda (x) x) (-> a a)) y) (hasheq 'a '* 'y 'a)) 'a)
+  (check-equal? (infer '(((: (lambda (x) (lambda (y) x))
                              (-> a (-> b a)))
                           i)
                          j)
