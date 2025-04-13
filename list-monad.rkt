98b5fc3410a002c26e0889d63ec85b6693aafe18
Mike Delmonaco <mthundershock@gmail.com>
2025-04-13 11:09:46 -0400

    for/fold/loop

--- list-monad.rkt
+++ list-monad.rkt
@@ -2,7 +2,26 @@
 
 ;; A different version of for*-like loops inspired by haskell's list monad.
 ;; It's essentially some nice syntax over generators and reducers.
+;; NOTE: mutually recursive definitions may not work in loop body.
+;; NOTE: loop uses generators internally, so yielding from inside the loop body will break stuff.
 
+;; TODO abstract away generator. use custom algebraic effect
+
+(provide
+ loop
+ loop/fold
+ ;; loop macros
+ unless
+ ;; collectors
+ sum
+ product
+ all
+ some
+ to-list
+ to-hash
+ to-set
+ collect-first
+ collect-last)
 (module+ test (require rackunit))
 (require racket/generator
          syntax-spec
@@ -38,7 +57,8 @@
   (nonterminal/nesting clause (nested)
     #:binding-space loop
     #:allow-extension loop-macro
-    (begin c:clause ...)
+
+    (begin/loop c:clause ...)
     #:binding (nest c ... nested)
 
     (when e:racket-expr)
@@ -58,15 +78,19 @@
     b:racket-body
     #:binding (scope (import b) nested))
   (host-interface/expression
-    (loop/host c:clause ...)
+    (loop/host #:arity arity:number c:clause ...)
     #:binding (nest c ... [])
     (define/syntax-parse (c^ ...) (flatten-begins (attribute c)))
-    #'(in-generator (let () (compile-clauses c^ ...)))))
+    #'(in-generator #:arity arity (let () (compile-clauses c^ ...)))))
 
 (define-syntax loop
   (syntax-parser
-    [(loop (~optional (~seq #:collector collector) #:defaults ([collector #'void])) c ...)
-     #'(collector (loop/host c ...))]))
+    [(loop (~alt (~optional (~seq #:collector collector) #:defaults ([collector #'void]))
+                 (~optional (~seq #:arity arity:number) #:defaults ([arity #'1])))
+           ...
+           c
+           ...)
+     #'(collector (loop/host #:arity arity c ...))]))
 
 (begin-for-syntax
   (define flatten-begins
@@ -74,21 +98,13 @@
       [() #'()]
       [(c0 c ...)
        (syntax-parse #'c0
-         #:literals (begin)
-         [(begin c^ ...)
+         ;; it's suboptimal that this has to be a datum literal
+         #:datum-literals (begin/loop)
+         [(begin/loop c^ ...)
           (flatten-begins #'(c^ ... c ...))]
          [_
           (define/syntax-parse (c^ ...) (flatten-begins #'(c ...)))
           #'(c0 c^ ...)])])))
-
-#|
-do
-  x <- xs
-  y < ys
-  return (x + y)
-~>
-xs >>= \x -> ys >>= \y -> [x + y]
-|#
 
 (define-syntax compile-clauses
   (syntax-parser
@@ -98,7 +114,7 @@ xs >>= \x -> ys >>= \y -> [x + y]
     [(_ (~or (when . _)
              (<- . _)))
      (raise-syntax-error 'list-monad "last clause must be a regular racket expression" this-syntax)]
-    [(_ e) #'(yield e)]
+    [(_ e) #'(call-with-values (lambda () e) yield)]
     [(_ c0 c ...)
      #'(compile-clause c0 (compile-clauses c ...))]))
 
@@ -119,36 +135,62 @@ xs >>= \x -> ys >>= \y -> [x + y]
 (define (yield* seq)
   (for ([v seq]) (yield v)))
 
-#;
-(define-dsl-syntax when loop-macro
-  (syntax-rules ()
-    [(when e)
-     [_ (if e (list #f) (list))]]))
 (define-dsl-syntax unless loop-macro
   (syntax-rules ()
     [(unless e)
      (when (not e))]))
 
-(define-syntax for/fold/loop
-  ;; TODO unclear how this should/could work
-  'todo)
+(define-syntax loop/fold
+  (syntax-parser
+    [(_ ([x init] ...) c ... final)
+     (define/syntax-parse (x^ ...) (generate-temporaries (attribute x)))
+     (define/syntax-parse arity (length (attribute x)))
+     #'(let ([x init] ...)
+         (loop
+          #:arity arity
+          ;; gives the last iteration's values
+          #:collector (lambda (seq) (for/fold ([x^ #f] ...) ([(x ...) seq]) (values x ...)))
+          (yield x ...)
+          c
+          ...
+          (let-values ([(x^ ...) (let () final)])
+            (set! x x^)
+            ...
+            (values x ...))))]))
 
-#;; this is what I want
-(for/fold/loop ([sum 0])
-  [x lst]
-  (+ sum x))
+(module+ test
+  (check-equal?
+   (loop/fold ([sum 0])
+     [x (in-range 5)]
+     (when (even? x))
+     (+ sum x))
+   6)
+  (test-case "fold with multiple values"
+    (define-values (sum product)
+      (loop/fold ([sum 0]
+                      [product 1])
+        [x (in-range 1 5)]
+        (when (even? x))
+        (values (+ sum x)
+                (* product x))))
+    (check-equal? sum 6)
+    (check-equal? product 8)))
 
 ;; collectors
-(define (sum seq) (for/sum ([x seq]) x))
+(define-syntax-rule (for->collector for/xyz)
+  (lambda (seq) (for/xyz ([x seq]) x)))
+(define sum (for->collector for/sum))
 (define (product seq)
   (for/fold ([product 1])
             ([x seq])
     (* product x)))
-(define (all seq) (for/and ([x seq]) x))
-(define (some seq) (for/or ([x seq]) x))
-(define (to-list seq) (sequence->list seq))
+(define all (for->collector for/and))
+(define some (for->collector for/or))
+(define to-list (for->collector for/list))
 (define (to-hash seq) (for/hash ([(k v) seq]) (values k v)))
-(define (to-set seq) (for/set ([x seq]) x))
+(define to-set (for->collector for/set))
+(define collect-first (for->collector for/first))
+(define collect-last (for->collector for/last))
 
 (module+ test
   (check-equal?
