4532a36759f20dba05d0a485fd40079f913a9cb3
Mike Delmonaco <mthundershock@gmail.com>
2025-04-26 17:32:56 -0400

    booleans for dependent types

--- dependent-types.rkt
+++ dependent-types.rkt
@@ -1,12 +1,20 @@
 #lang racket
 
 ; a little type checker/interpreter for a dependently typed language
-; taken from https://www.andres-loeh.de/LambdaPi/LambdaPi.pdf
+; initially taken from https://www.andres-loeh.de/LambdaPi/LambdaPi.pdf
+; but expanded on in several ways
 
-; TODO add booleans
 ; TODO add naturals
 ; TODO add vectors
-; TODO add phasing so there is an environment for evaluations during type checking
+; TODO add staging so there is an environment for evaluations during type checking
+#;
+(let-for-type ([x e])
+  (define y : (x Number)
+    ...))
+;; TODO even better, support that with just let. there'd be no separation between checking-time and evaluation-time
+;; Just-in-time type checking lol
+;; you'll need types as values for (let ([x Bool]) (: #t x))
+;; TODO more convenient syntax for lambda annotation
 
 ;; surface syntax
 
@@ -18,28 +26,35 @@
 ; (lambda (symbol) Expr)
 ; (let [(symbol Expr)] Expr)
 ; (Expr Expr)
+; #t
+; #f
+; (if Expr Expr Expr)
+; (boolElim Expr Expr Expr Expr)   (dependent if)
 ; Represents the surface syntax of a program as an s-expression
 
 ; The meaning of (forall (: x t1) t2) is a function type analogous to (t1 -> t2).
+; It can more clearly be written as (x : t1) -> t2
 ; t1 is the type of value that gets passed in for x. t2 is the return type.
 ; But t2 can depend on the _value_ of x.
-; t1 and t2 both have to be of kind *. Remember * is of kind *.
-; Example: (forall (: x Int) Bool) is (Int -> Bool)
-; Example: (forall (: a *) (forall (: x a) a))
-;   is the type of the identity function, but it has to be instantiated with a type like ((id Nat) 1) ~> 1.
-; Example: (forall (: a *) (forall (: b *) (forall (: x a) (forall (: y b) a))))
-;   is the type of the const function (a -> (b -> a))
-; Example: (forall (: a *) a)
-;   is the type of a function that takes in a type and returns a value of that type (impossible)
-; Example: (forall (: a *) (forall (: n Nat) (forall (: x a) (Vec n a))))
-;   is the type of a function that takes in a vector length and a value and
-;   creates a vector of that length filled with copies of the value.
-;   Assuming Nat is of type *.
+; Dependent example:
+; (n : Nat) -> StringVec n
+; (forall (: n Nat) (StringVec n))
+; that takes in a natural number and then returns a string vector of that length, which is statically checked.
+; ordinary functions like Nat -> Bool are a special case where the return type
+; does not depend on the argument value: (forall (: x Nat) Bool)
+; more examples
+; identity : (forall (: a *) (forall (: x a) a))
+; requires explicit type application: ((identity Bool) #t) ~> #t
+; const : (forall (: a *) (forall (: b *) (forall (_ : a) (forall (_ : b) a))))
+; ((((const Bool) Nat) #t) 1) ~> #t
 
 ;; runtime
 
 ; A Value is one of
+; #t
+; #f
 ; '*                                (the kind of types)
+; 'Bool                             (the type of booleans)
 ; (Value -> Value)                  (function value)
 (struct pi [t-x x->t-ret]);         (dependent function type)
 ;; where
@@ -53,7 +68,11 @@
 
 ; A NeutralExpr is one of
 ; symbol
-; (list NeutralExpr Value)
+; (list NeutralExpr Value)                        (application)
+; (list 'if NeutralExpr Value Value)
+; (list 'boolElim Expr NeutralExpr Value Value)   (dependent if)
+;;                ^ expr because it's only relevant to type-checking
+; Represents an irreducible expression
 
 ; An Env is a (hash symbol Value)
 (define empty-env (hasheq))
@@ -64,15 +83,38 @@
 
 (module+ test (require (except-in rackunit check)))
 
+;; Expr Ctx Env -> Expr
+;; infers the type and evaluates it
+(define (run expr ctx env)
+  (define t (quote-value (infer expr ctx)))
+  (define v (normalize expr env))
+  `(: ,v ,t))
+
+(module+ test
+  (check-equal? (run '(if #t #t #f) empty-ctx empty-env)
+                '(: #t Bool))
+  (check-equal? (run '(: (lambda (x) x)
+                         (forall (: x Bool) Bool)) empty-ctx empty-env)
+                '(: (lambda (_.0) _.0)
+                    (forall (: _.0 Bool) Bool))))
+
 ; Expr -> Expr
-(define (normalize expr) (quote-value (eval expr empty-env)))
+; reduce an expression to its canonical form.
+; note that the result is an expression.
+; any beta or alpha equivalent expressions should normalize to the same expression.
+(define (normalize expr [env empty-env]) (quote-value (eval expr env)))
+
+(module+ test
+  (check-equal? (normalize '(let ([x (lambda (y) y)]) (x x)))
+                '(lambda (_.0) _.0)))
 
 ; Expr -> Value
 (define (eval expr env)
   (match expr
     ['* '*]
     [(? symbol? x) (hash-ref env x x)]
-    [`(: ,expr _) (eval expr env)]
+    [(? boolean? b) b]
+    [`(: ,expr ,_) (eval expr env)]
     [`(lambda (,x) ,body)
      (lambda (v-x) (eval body (hash-set env x v-x)))]
     [`(let [(,x ,rhs)] ,body)
@@ -84,6 +126,28 @@
      ; p-ret can depend on the _value_ of x
      (define t-x (eval p-x env))
      (pi t-x (lambda (v-x) (eval p-ret (hash-set env x v-x))))]
+    [`(if ,cnd ,thn ,els)
+     (match (eval cnd env)
+       [#t (eval thn env)]
+       [#f (eval els env)]
+       [n
+        ;; neutral case, evaluate branches as much as we can
+        ;; we're type-checked, so we don't have to worry about
+        ;; it being a non-bool!
+        `(if ,n
+             ,(eval thn env)
+             ,(eval els env))])]
+    [`(boolElim ,m ,cnd ,thn ,els)
+     (match (eval cnd env)
+       [#t (eval thn env)]
+       [#f (eval els env)]
+       [n
+        ;; neutral case, evaluate branches as much as we can
+        ;; we're type-checked, so we don't have to worry about
+        ;; it being a non-bool!
+        `(boolElim ,m ,n
+           ,(eval thn env)
+           ,(eval els env))])]
     [`(,rator ,rand)
      (match (eval rator env)
        [(and f (? procedure?)) (f (eval rand env))]
@@ -93,36 +157,47 @@
         `(,n ,(eval rand env))])]))
 
 ; Value -> Expr
-(define (quote-value v [count 0])
-  (match v
-    ['* '*]
-    [(? symbol?) v]
-    [`(,n-rator ,v-rand)
-     `(,n-rator ,(quote-value v-rand count))]
-    [(pi t-arg arg->t-ret)
-     (define x (number->var count))
-     ; you should probably use mutation for count.
-     ; you probably want to thread the state through for these two recursive calls.
-     ; technically doesn't matter, but still a little weird.
-     `(forall (: ,x ,(quote-value t-arg count))
-              ,(quote-value (arg->t-ret x) (add1 count)))]
-    [(? procedure?)
-     (define x (number->var count))
-     `(lambda (,x) ,(quote-value (v x) (add1 count)))]))
+(define (quote-value v)
+  (define count 0)
+  ;; generates values like '_.0, '_.1, ...
+  (define (my-gensym)
+    (begin0 (number->var count)
+      (set! count (add1 count))))
+  (let loop ([v v])
+    (match v
+      ['* '*]
+      [(? symbol?) v]
+      [(? boolean?) v]
+      [`(,n-rator ,v-rand)
+       `(,(loop n-rator) ,(loop v-rand))]
+      [`(boolElim ,m ,n-cnd ,v-thn ,v-els)
+       `(boolElim ,m ,(loop n-cnd) ,(loop v-thn) ,(loop v-els))]
+      [(pi t-arg arg->t-ret)
+       (define x (my-gensym))
+       `(forall (: ,x ,(loop t-arg))
+                ,(loop (arg->t-ret x)))]
+      [(? procedure?)
+       (define x (my-gensym))
+       `(lambda (,x) ,(loop (v x)))])))
 
 (module+ test
   (check-equal? (quote-value 'x) 'x)
   (check-equal? (quote-value '(x y)) '(x y))
+  (check-equal? (quote-value `((x ,(lambda (y) y)) z))
+                             '((x (lambda (_.0) _.0)) z))
+  (check-equal? (quote-value `(boolElim m b ,(lambda (x) x) ,(lambda (x) #t)))
+                             '(boolElim m b (lambda (_.0) _.0) (lambda (_.1) #t)))
   (check-equal? (quote-value (lambda (x) x)) '(lambda (_.0) _.0))
   (check-equal? (quote-value (lambda (x) (lambda (y) x)))
                 '(lambda (_.0) (lambda (_.1) _.0)))
   (check-equal? (quote-value '*) '*)
   ; type of the identity function
-  ; (forall (: a *) (forall (: x a) x))
-  (check-equal? (quote-value (pi '* (lambda (v) (pi v (lambda (w) w)))))
-                '(forall (: _.0 *) (forall (: _.1 _.0) _.1))))
+  ; (forall (: a *) (forall (: x a) a))
+  (check-equal? (quote-value (pi '* (lambda (a) (pi a (lambda (x) a)))))
+                '(forall (: _.0 *) (forall (: _.1 _.0) _.0))))
 
-; Natural -> symbol
+;; Natural -> symbol
+;; like minikanren
 (define (number->var n)
   (string->symbol (format "_.~a" n)))
 
@@ -133,8 +208,10 @@
 (define (infer expr ctx)
   (match expr
     ['* '*]
-    [(? symbol? expr)
-     (hash-ref ctx expr (lambda () (error 'infer "variable not found in context")))]
+    ['Bool '*]
+    [(? symbol? x)
+     (hash-ref ctx x (lambda () (error 'infer "unbound variable: ~a" x)))]
+    [(? boolean? b) 'Bool]
     [`(: ,e ,p)
      (check p '* ctx)
      (define t (eval p empty-env))
@@ -149,6 +226,23 @@
      (define t-rhs (infer rhs ctx))
      (infer body (hash-set ctx x t-rhs))]
     [(list lambda _ _) (error 'infer "cannot infer type of lambda")]
+    [`(if ,cnd ,thn ,els)
+     (check cnd 'Bool ctx)
+     (define t (infer thn ctx))
+     (check els t ctx)
+     t]
+    [`(boolElim ,m ,cnd ,thn ,els)
+     ;; conceptually,
+     ;; boolElim : (m : Bool -> *) (cnd : Bool) (thn : m #t) (els : m #f) -> m cnd
+     ;; where m is called the "motive"
+     ;; it's a way for the type to depend on the value condition
+     (check m (pi 'Bool (lambda (_) '*)) ctx)
+     (define b->t (eval m empty-env))
+     (check cnd 'Bool ctx)
+     (check thn (b->t #t) ctx)
+     (check els (b->t #f) ctx)
+     (define b (eval cnd empty-env))
+     (b->t b)]
     [`(,rator ,rand)
      (define t-rator (infer rator ctx))
      (match t-rator
@@ -183,21 +277,21 @@
           (quote-value v2)))
 
 (module+ test
-  (define ctx (hasheq 'Bool '* 'true 'Bool 'false 'Bool))
+  (define ctx (hasheq 'unit 'Unit 'Unit '*))
   (check-equal? (eval 'x empty-env) 'x)
   (check-equal? (eval 'x (hasheq 'x 'y)) 'y)
   (check-equal? (normalize '(lambda (x) x)) '(lambda (_.0) _.0))
   (check-equal? (eval '((lambda (x) x) y) empty-env) 'y)
   (check-equal? (eval '(let ([x y]) x) empty-env) 'y)
-  (check-equal? (infer 'true ctx) 'Bool)
-  (check-equal? (check 'true 'Bool ctx) (void))
+  (check-equal? (infer #t ctx) 'Bool)
+  (check-equal? (check #t 'Bool ctx) (void))
   (check-equal? (check '(lambda (x) x) (pi 'Bool (const 'Bool)) ctx) (void))
   (check-equal? (quote-value
                  (infer '(: (lambda (x) x)
                             (forall (: x Bool) Bool))
                         ctx))
                 '(forall (: _.0 Bool) Bool))
-  (check-equal? (infer '((: (lambda (x) x) (forall (: x Bool) Bool)) true) ctx) 'Bool)
+  (check-equal? (infer '((: (lambda (x) x) (forall (: x Bool) Bool)) #t) ctx) 'Bool)
   (check-equal? (infer '(((: (lambda (x) (lambda (y) x))
                              (forall (: x a) (forall (: y b) a)))
                           i)
@@ -211,7 +305,7 @@
   (check-equal? (infer '(((: (lambda (a) (lambda (x) x))
                              (forall (: a *) (forall (: x a) a)))
                           Bool)
-                         true)
+                         #t)
                        ctx)
                 'Bool)
   (check-equal? (check '(: (lambda (a) (lambda (x) x))
@@ -221,6 +315,93 @@
                 (void))
   (check-equal? (infer '(let ([id (: (lambda (a) (lambda (x) x))
                                      (forall (: a *) (forall (: x a) a)))])
-                          ((id Bool) true))
+                          ((id Bool) #t))
                        ctx)
-                'Bool))
+                'Bool)
+  ;; if
+  (check-equal? (run '(if #t unit unit) ctx empty-env)
+                '(: unit Unit))
+  (check-equal? (infer '(boolElim (lambda (b) Unit) #t unit unit)
+                       ctx)
+                'Unit)
+  ;; non-trivial dependent if
+  (check-equal? (run '(boolElim (lambda (b) (boolElim (lambda (b) *) b Unit Bool)) #t unit #f)
+                     ctx
+                     empty-env)
+                '(: unit Unit))
+  ;; the other branch
+  (check-equal? (run '(boolElim (lambda (b) (boolElim (lambda (b) *) b Unit Bool)) #f unit #t)
+                     ctx
+                     empty-env)
+                '(: #t Bool))
+  ;; neutral condition
+  (check-equal? (infer '(boolElim (lambda (b) (boolElim (lambda (b) *) b Unit Bool)) cnd unit #t)
+                       (hash-set ctx 'cnd 'Bool))
+                '(boolElim (lambda (b) *) cnd Unit Bool))
+  (check-equal? (eval '(boolElim m #t a b)
+                      empty-env)
+                'a)
+  (check-equal? (eval '(boolElim m b t e) empty-env)
+                '(boolElim m b t e))
+  ;; closures work with neutrals by normalized substitution
+  (check-equal? (normalize '(let ([x #t])
+                              (: (lambda (y) x)
+                                 (forall (: z Unit) Boolean))))
+                '(lambda (_.0) #t))
+  ;; type checking really does normalize
+  (check-equal? (quote-value
+                 (infer '(: (: (lambda (x) x)
+                               (forall (: y Bool) Bool))
+                            (forall (: z Bool) Bool))
+                        ctx))
+                '(forall (: _.0 Bool) Bool))
+  ;; for jit
+  #;(check-equal? (infer '(let ([X Bool]) (: #t X)) ctx)
+                  ;; or should it be X?
+                  'Bool))
+
+#|
+From talk with michael:
+The reason we need neutrals is
+you'll end up having things that can't be reduced,
+but the checker will need to prove they're equal. but it's just syntactic equality which kind of sucks.
+there is a tradeoff between how much equality the type checker does and how much burden goes on the prover.
+On one end, you have the Eq a a type that you have to prove,
+on the other you have syntactic equality automatically done by the checker.
+|#
+
+#|
+adding booleans
+e := ...
+   | True
+   | False
+   | boolElim e_motive e_cnd e_thn e_els
+
+t := ...
+   | 'Bool
+
+boolElim has type (m : Bool -> *) -> (b : Bool) -> m True -> m False -> -> m b
+the motive tells you what the result type is for true and false. For example:
+
+def boolElim {m : Bool → Type} (b : Bool) (thn: m true) (els: m false) : m b :=
+  match b with
+   | true => thn
+   | false => els
+def m (b : Bool) := if b then Nat else String
+def natFromBoolElim := @boolElim m true (0 : Nat) "hello"
+
+funny thing: to define a non-trivial motive (m : Bool -> *), you need to use boolElim
+
+eval(#t) = #t
+eval(#f) = #f
+eval(boolElim m e-cnd e-thn e-els) = eval(e-thn) when eval(e-cnd) = #t
+eval(boolElim m e-cnd e-thn e-els) = eval(e-els) when eval(e-cnd) = #f
+;; neutral case
+eval(boolElim m e-cnd e-thn e-els) = boolElim m n v-thn v-els
+  when
+    eval(e-cnd) = n
+    eval(e-thn) = v-thn
+    eval(e-els) = v-els
+;; this is weird because we evaluate both branches in the neutral case,
+;; but ig it's no weirder than pre-evaluating function bodies
+|#
